88afa33d330c5e48ce22b33a0c3f1530687180f4
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-21 04:05:14 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,11 @@
 def hello_t(name)
   name.each {|name| puts name} 
-#yield
-#{ |name| puts name.upcase } 
+yield
+{|name| puts name.upcase} 
 #  "Hey! No block was given!"
   
 
 end
 # call your method here!
+hello_t
 
